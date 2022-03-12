38dd114557bc816607d9dc9832a8bc6a6979fc8c
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2022-03-12 11:32:50 +0900

    20220312r1

---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+
+class AppColor{
+  static Color homePageBackground = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xff0f17ad);
+  static Color gradientSecond = const Color(0xff6985e8);
+  static Color homePageTitle = const Color(0xFF302f51);
+  static Color homePageSubtitle = const Color(0xFF414160);
+  static Color homePageDetail = const Color(0xFF6588f4);
+  static Color homePageIcons = const Color(0xFF3b3c5c);
+}
