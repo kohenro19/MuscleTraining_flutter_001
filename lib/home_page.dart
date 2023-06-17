5f26ce9b892b796355c17195b0ae1cbb99effa07
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2023-06-17 23:36:14 +0900

    test ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,156 +14,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: color.AppColor.homePageBackground,
-      body: Container(
-        padding: const EdgeInsets.only(top:70, left: 30, right: 30),
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            color: Colors.orange,
+          ),
+          Container(
+            height: 100,
+            color: Colors.pink,
+          ),
+          Row(
               children: [
-                Text(
-                  "Training",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: color.AppColor.homePageTitle,
-                    fontWeight: FontWeight.w700
+                Expanded( // Expanded ウィジェットを使用することで、子ウィジェットが利用可能なスペースを最大限に活用し、残りのスペースを均等に分割することができる
+                  child: Container(
+                    height: 100,
+                    color: Colors.green,
                   ),
                 ),
-                Expanded(child: Container()),
-                Icon(Icons.arrow_back_ios,
-                size: 20,
-                color: color.AppColor.homePageIcons
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    color: Colors.pink,
+                  ),
                 ),
-                SizedBox(width: 10,),
-                Icon(Icons.calendar_today_outlined,
-                size: 20,
-                color: color.AppColor.homePageIcons,
-                ),
-                SizedBox(width: 10,),
-                Icon(Icons.arrow_forward_ios,
-                size: 20,
-                color: color.AppColor.homePageIcons
-                )
+                Container(
+                    height: 100,
+                    width: 20,
+                    color: Colors.black12,
+                  )
+                
               ],
             ),
-            SizedBox(height: 30,),
-            Row(
-              children: [
-                Text(
-                  "Your Program",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: color.AppColor.homePageTitle,
-                    fontWeight: FontWeight.w700
-                  ),
-                ),
-                Expanded(child: Container()),
-                Text(
-                  "Details",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: color.AppColor.homePageDetail,
-                  ),
-                ),
-                SizedBox(width: 5,),
-                Icon(Icons.arrow_forward,
-                size: 20,
-                color:color.AppColor.homePageIcons
-                )
-              ],
-              
-            ),
-            SizedBox(height: 20,),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  color.AppColor.gradientFirst.withOpacity(0.8),
-                  color.AppColor.gradientSecond.withOpacity(0.9)
-                ],
-                begin: Alignment.bottomLeft,
-                end: Alignment.centerRight
-              ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                    topRight: Radius.circular(80)
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(5,10),
-                      blurRadius: 10,
-                      color: color.AppColor.gradientSecond.withOpacity(0.2)
-                    )
-                  ]
-              ),
-              child: Container(
-                padding: const EdgeInsets.only(left: 20, top: 25, right: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Next workout",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: color.AppColor.homePageContainerTextSmall
-                       ),
-                     ),
-                    SizedBox(height: 5,),
-                    Text(
-                        "Legs Toining",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: color.AppColor.homePageContainerTextSmall
-                        ),
-                    ),
-                    SizedBox(height: 5,),
-                    Text(
-                        "and Glutes Workout",
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: color.AppColor.homePageContainerTextSmall
-                        ),
-                    ),
-                    SizedBox(height: 25),
-                    // Row(
-                    //   // crossAxisAlignment: CrossAxisAlignment.end,
-                    //   children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.timer,
-                              size: 20,
-                              color: color.AppColor.homePageContainerTextSmall
-                            ),
-                            Text(
-                                "60 min",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: color.AppColor.homePageContainerTextSmall
-                                )
-                            ),
-                            Expanded(child: Container()),
-                            Container(
-                              child: Icon(
-                              Icons.play_circle_fill,
-                              color: Colors.white,
-                              size: 60,
-                              )
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                  // ]
-                ),
-              ),
-            // ),
-          ],
-        ),
-      ),
+        ],
+
+      )
     );
   }
 }
